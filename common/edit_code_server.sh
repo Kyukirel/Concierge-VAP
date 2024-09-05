8f2b8eb41cc8da36d96e4139d0fae66c511bde42
Rafael Sinjunatha Wulangsih <89903270+Rafael-SW048@users.noreply.github.com>
2024-09-06 02:12:39 +0700

    Update edit_code_server.sh

--- common/edit_code_server.sh
+++ common/edit_code_server.sh
@@ -20,13 +20,21 @@ correct_code() {
     fi
 }
 
-# Update the IP address in envTest.sh and run_zharfanf.sh
+# Paths to the configuration files
 ENVTEST_PATH=/tmp/ramdisk/VAP-Concierge/src/envTest.sh
 RUN_ZHARFANF_PATH=/tmp/ramdisk/VAP-Concierge/src/run_zharfanf.sh
 DEFAULT_CLIENT_IP="10.140.83.30"
-read -p "Enter the IP address to use (or press Enter to use the default [$DEFAULT_CLIENT_IP]): " USER_INPUT_IP
-CLIENT_IP=${USER_INPUT_IP:-$DEFAULT_CLIENT_IP}
-echo "Using IP address: $CLIENT_IP"
+
+# Check if an IP is passed as an argument, otherwise prompt for user input
+if [ -n "$1" ]; then
+    CLIENT_IP="$1"
+    echo "Using IP address passed as argument: $CLIENT_IP"
+else
+    read -p "Enter the IP address to use (or press Enter to use the default [$DEFAULT_CLIENT_IP]): " USER_INPUT_IP
+    CLIENT_IP=${USER_INPUT_IP:-$DEFAULT_CLIENT_IP}
+    echo "Using IP address: $CLIENT_IP"
+fi
+
 # Search for the first eligible IP address in the envTest.sh file and store it in ORIGINAL_IP
 ORIGINAL_IP=$(grep -oP '(?<=http://)\d{1,3}(\.\d{1,3}){3}(?=:6000/)' $ENVTEST_PATH | head -n 1)
 if [ -z "$ORIGINAL_IP" ]; then
@@ -34,8 +42,7 @@ if [ -z "$ORIGINAL_IP" ]; then
     exit 1
 fi
 echo "Original IP found: $ORIGINAL_IP"
+
 # Replace the original IP with the new CLIENT_IP in envTest.sh and run_zharfanf.sh
 correct_code "$ENVTEST_PATH" "s|$ORIGINAL_IP|$CLIENT_IP|g" "Update the IP address in envTest.sh from $ORIGINAL_IP to $CLIENT_IP"
 correct_code "$RUN_ZHARFANF_PATH" "s|$ORIGINAL_IP|$CLIENT_IP|g" "Update the IP address in run_zharfanf.sh from $ORIGINAL_IP to $CLIENT_IP"
-
-
